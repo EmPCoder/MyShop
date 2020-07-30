acebcb56b1000043ac63de1f1ce0d5237cfc379f
Matthew Hudock <Matthewd1715@gmail.com>
2020-07-30 11:28:53 -0400

    Added Support for the SQL DB to the project

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")//Causes the class to look in web.config for a section called "DefaultConnection"Can also go in App.config
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
